3562cfc3874481f82d106be92640185131411871
Mister1Victor <victarslab@gmail.com>
2026-07-14 14:38:48 +0300

    v0.2.7 Create knowledge loader

---
+++ core/knowledge/loader.py
@@ -0,0 +1,15 @@
+import os
+
+from langchain_community.document_loaders import (
+    PyPDFLoader,
+    Docx2txtLoader,
+)
+
+from langchain_text_splitters import (
+    RecursiveCharacterTextSplitter,
+)
+
+def load_knowledge_base():
+
+    pass
+
